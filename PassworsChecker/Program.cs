28e1b1bee31cba639b5b136f8becc234c67befe9
I-Do-CS <150284763+I-Do-CS@users.noreply.github.com>
2024-05-31 15:05:50 +0330

    finished app

--- PassworsChecker/Program.cs
+++ PassworsChecker/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace PasswordChecker
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Ask user for a password
+            Console.Write("Enter Password: ");
+            string? password = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(password))
+            {
+                Console.WriteLine("Invalid Password, Try again.");
+                Main();
+                return;
+            }
+
+            Console.Write("Confirm Password: ");
+            string? confirmPassword = Console.ReadLine();
+
+            if (password.Equals(confirmPassword))
+            {
+                Console.WriteLine("Passwords Match. Thanks!");
+            }
+            else
+            {
+                Console.WriteLine("Passwords do not match, Try again.");
+                Main();
+                return;
+            }
+
+            return;
+        }
+    }
+}
